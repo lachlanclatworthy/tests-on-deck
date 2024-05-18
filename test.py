c2d16922369d3d10d6160ec13fe187c790ccb8ce
Lachlan Clatworthy <lachlanclatworthy@proton.me>
2024-05-18 13:47:06 +1000

    Update test.py to support multiple test suites

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
-#!python
+#!python3
 
 from os import system
 
-system("bazel test --cxxopt=-std=c++14 --test_output=all //:hello_test")
+tests = [
+        "hello_test",
+]
+
+# //:test_1 ....
+def test_str(tests: list) -> str:
+    out = ""
+    for t in tests:
+        out = out + "//:{} ".format(t)
+    return out
+
+
+system(f"bazel test --cxxopt=-std=c++14 --test_output=all {test_str(tests)}")
